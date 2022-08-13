74257fb56782109512a161a443fabc9b6cb54996
ajaikamble10 <ajaysuhaskamble@gmail.com>
2022-08-13 12:13:36 +0530

    api added single view

--- src/main/java/in/crudapp/springboot/controller/StudentController.java
+++ src/main/java/in/crudapp/springboot/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +32,10 @@ public class StudentController {
 	@PostMapping("/students")
 	public ResponseEntity<Student> createStudent(@RequestBody Student stud){
 		return new ResponseEntity<Student>(studRepo.save(stud),HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/students/{id}")
+	public ResponseEntity<Student> getStudent(@PathVariable Long id){
+		return new ResponseEntity<Student>(studRepo.findById(id).get(),HttpStatus.OK);
 	}
 }
